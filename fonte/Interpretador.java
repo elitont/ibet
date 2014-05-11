c21c3e1c556689c7a61a4cdac20776a280a2a89a
igor beilner <igor.bielner@hotmail.com>
2014-05-11 17:43:51 -0300

    breqr

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -14,35 +14,36 @@ class Interpretador {
     private String[] linhas;
     private Opera op;
 
+    public Interpretador(){
+        op = new Opera();
+    }
+
     public void interpreta(String[] l) {
         this.linhas = l;
         
         for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != null) {	//testar 
-                if(i == 0 && this.linhas[0].equals("Inicio") != true){
+                if(i == 0 && this.linhas[0].equals("Inicio") == false){
                     op.erro(0);
-                } 
+                }
                 tokens(this.linhas[i]);
                 //System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
             }
         }
     }
-   public void tokens(String a){
+   public int tokens(String a){
         String[] b;
+
         b = a.split("[|]");
         int i;
         for(i = 0; i < b.length; i++){
             System.out.println(b[i]);
         }
-        //comandos(b);
+        
+        if(b[0].equals("double")) op.criaVariavel(b[1]); 
+
+        return 1;
     }
-
-   /* public void comandos(String[] a){   
-            if(a[0].equals("double")){
-                op.criaVariavel(a[1]);
-            }
-    }*/
-
 
 
 
