5042711f5646bab19cb96cb7e7cba7e3dc1502f5
igorbeilner <igor.beilner@hotmail.com>
2014-05-25 11:14:59 -0300

    enquanto funcionando comentado

--- Fonte/Interpretador.java
+++ Fonte/Interpretador.java
@@ -1,24 +1,19 @@
-/**
- * Exemplo de interpretador.
- *
- * Esse código é um exemplo de interpretador para a linguagem 'Blah'. Esse programa
- * não faz qualquer interpretação, ele apenas lê o conteúdo de um arquivo que foi
- * passado pela linha de comando.
- *
- * Originalmente por Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
- * Modificado por: Eliton Traverssini e Igor Beilner
+/** 
+ * Por: Eliton Traverssini e Igor Beilner
  * <eliton.traverssini@gmail.com> <igor.beilner@hotmail.com>
  */
-//teste smart git
 class Interpretador {
     private String[] linhas;
     private Opera op;
-    private int abre, fecha;	//escopos gerais
-    private int open, close;	//escopo se falso
-    private int on, off;		//escopo laço falso
-    private static int flag; //condiçao falsa se
-    private static int flag2; //condiçao falsa laço
-
+    private int abre, fecha;	        //escopos gerais
+    private int open, close;	        //escopos "se", quando falso
+    private int on, off;		        //escopo "enquanto", quando falso
+    private static int flag_se;         //condiçao falsa do "se"
+    private static int flag_laco;       //condiçao falsa do "enquanto"
+    private static int flag_laco_F;     //condiçao falsa do "enquanto"
+    private int swap[] = new int[50];
+    private int swap2[] = new int[50];
+    private int posicao_pilha;
 
     public Interpretador(){
         op = new Opera();
@@ -28,60 +23,85 @@ class Interpretador {
         this.close = 0;
         this.on = 0;
         this.off = 0;
-        flag = 0;
-        flag2 = 0;
+        flag_se = 0;
+        flag_laco = 0;
+        flag_laco_F = 0;
+        posicao_pilha = 0;
     }
 
     public void interpreta(String[] l) {
         this.linhas = l;
-        int swap = 0, swap2 = 0;
+        
         for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != null) {    //testar 
-            	if(flag2 == 0 && this.linhas[i].contains("enquanto|")){
-            		swap = i;
+                //coloca a posição do enquanto na pilha quando a condição for verdadeira
+            	if(flag_laco == 0 && this.linhas[i].contains("enquanto|")){
+            		swap[posicao_pilha] = i;
+                    //posicao_pilha++;
             	}
-            	if(this.linhas[i].contains("se|") || this.linhas[i].contains("enquanto|") || this.linhas[i].contains("Inicio")) {
+                //------------------------------------------------------------------------------------------------------------------
+            	//conta todos os escopos que foram abertos e fechados e verifica se todos que foram abertos, foram fechados
+                if(this.linhas[i].contains("se|") || this.linhas[i].contains("enquanto|") || this.linhas[i].contains("Inicio")) {
             		this.abre++;
         		}
         		if(this.linhas[i].contains("fim_se") || this.linhas[i].contains("fim_enquanto") || this.linhas[i].contains("Fim")) {
             		this.fecha++;
         		}
-                if(i == 0 && this.linhas[0].equals("Inicio") == false){     //se nao contem Inicio da pau
+                //------------------------------------------------------------------------------------------------------------------
+                //verifica se a primeira linha de codigo contém Inicio, que equivale ao escopo da main
+                if(i == 0 && this.linhas[0].equals("Inicio") == false){
                     op.erro(0);
                 }
+                //apaga o flag de falso e zera o numero de escopos que foram abertos e fechados e desconsidera o que tem
+                //dentro deles
                 if(this.linhas[i].contains("fim_se") && (open == close)){
-                	flag = 0;
+                	flag_se = 0;
                 	open = close = 0;
                 }
-                if(this.linhas[i].contains("imprima") == false){            //deixa os espaços de impressao
+                //desconsidera a linha que contém imprima, para não retirar os espaços contidos na frase que será escrita
+                if(this.linhas[i].contains("imprima") == false){
                     this.linhas[i] = this.linhas[i].replace("\t","");
                     this.linhas[i] = this.linhas[i].replace(" ", "");
                 }
-                if(flag == 1 && this.linhas[i].contains("se|")){
+                //---------------------------------------------------------------------------------------------------------
+                //quando a condição de um "se" for falsa é necessário contar os escopos de "se" anihados para desconsiderar
+                //apenas o código que está dentro do "se" que deu condição falsa
+                if(flag_se == 1 && this.linhas[i].contains("se|")){
                 	open++;
                 }
-                if((flag == 1) && this.linhas[i].contains("fim_se")){
+                if((flag_se == 1) && this.linhas[i].contains("fim_se")){
                 	close++;
                 }
-                if(flag2 == 1 && this.linhas[i].contains("enquanto|")){
+                //---------------------------------------------------------------------------------------------------------
+                //quando a condição de um "enquanto" for falsa é necessário contar os escopos de "enquanto" anihados para desconsiderar
+                //apenas o código que está dentro do "enquanto" que deu condição falsa
+                if(flag_laco == 1 && this.linhas[i].contains("enquanto|")){
                 	on++;
                 }
-                if((flag2 == 1) && this.linhas[i].contains("fim_enquanto")){
-                	flag2 = 0;
-                	on = off = 0;
-                	off++;
-                	i = swap2+1;
+                if(flag_laco == 1 && this.linhas[i].contains("enquanto|")){
+                    off++;
                 }
+                //---------------------------------------------------------------------------------------------------------
+                //salta o código que está dentro do laço quando a condição for falsa
+                if((flag_laco == 1) && this.linhas[i].contains("fim_enquanto") && on == off){
+                	flag_laco = 0;
+                	on = off = 0;
+                	if(flag_laco_F == 0) i = swap2[posicao_pilha]+1;
+                    flag_laco_F = 0;
+                }
+                //retorna para a linha do "enquanto" para testar a condição novamente
                 else if(this.linhas[i].contains("fim_enquanto") && (on == off)){
-                	flag2 = 0;
+                	flag_laco = 0;
                 	on = off = 0;
-                	swap2 = i;
-                	i = swap;
+                	swap2[posicao_pilha] = i;
+                	i = swap[posicao_pilha];
+                    //posicao_pilha--;
                 }
-                if(flag == 0 && flag2 == 0) tokens(this.linhas[i]);
+                //invoca o método tokens quando quando os flags das condições dos "se" e "enquanto" forem verdadeiras
+                if(flag_se == 0 && flag_laco == 0) tokens(this.linhas[i]);
             }
         }
-        if(this.abre != this.fecha && swap == 0) {
+        if(this.abre != this.fecha && posicao_pilha != 0) {
             op.erro(9);
         }
     }
@@ -101,15 +121,20 @@ class Interpretador {
             op.impressao(b[0], b[1]);
         }
         else if(b[0].equals("se")){
-            if(op.condicao(b) == false) flag = 1;
+            if(op.condicao(b) == false) flag_se = 1;
         }
         else if(b[0].equals("enquanto")){
-        	if(op.condicao(b) == false) flag2 = 1;
+        	if(op.condicao(b) == false) {
+                flag_laco = 1;
+                flag_laco_F = 1;
+            }
         }
         else if(b[0].equals("Inicio") == false && b[0].equals("Fim") == false && b[0].equals("fim_se") == false && b[0].equals("fim_enquanto") == false){     //atribuição (sem cmd especial)
+            //length = 4 quando for atribuição e = 6 quando tiver operação
             if(b.length == 4 || b.length == 6){
                 op.atribuicao(b);
             }
+            //acusa erro quando a atribuição não estiver completa
             else{
                 op.erro(3);
             }
